8baaee909e01244ff46fe095e49a6d01e66c30a0
MelodyMoerlins <melody.moerlins@cvtechonline.net>
2025-03-04 09:53:12 -0600

    Add files via upload

---
+++ Python/guessinggame.py
@@ -0,0 +1,19 @@
+import random
+
+low = input("You want your number to guess to be in between: ")
+high = input("and: ")
+
+number = int(random.uniform(int(low), int(high)))
+
+guess = int(input(f"What's your first guess for a random number between {low} and {high}? "))
+
+while guess != number:
+    if number > guess:
+        lower = "higher"
+    else:
+        lower = "lower"
+    print(f"That's wrong. Your number is {lower}")
+    guess = int(input(f"What's your first guess for a random number between {low} and {high}? "))
+
+if number == guess:
+    print("Congratulations! You won!")
